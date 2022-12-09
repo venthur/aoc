1269ea1786aaadac83fa53893bd428f575314445
Bastian Venthur <venthur@debian.org>
2022-12-09 20:03:44 +0100

    2022/09 pt 2

--- 2022/09/main.py
+++ 2022/09/main.py
@@ -9,6 +9,18 @@ L 5
 R 2
 """.splitlines()
 
+test_input2 = """\
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+""".splitlines()
+
+
 with open('input.txt') as fh:
     input_ = fh.read().splitlines()
 
@@ -17,7 +29,6 @@ def task1(input_):
     visited = set()
     xt, yt = 0, 0
     xh, yh = 0, 0
-
 
     for line in input_:
         direction, steps = line.split()
@@ -68,5 +79,93 @@ def task1(input_):
     return len(visited)
 
 
+def task2(input_):
+    visited = set()
+    # 0 is head, 9 is tail
+    knots = [[0, 0] for i in range(10)]
+
+    for line in input_:
+        direction, steps = line.split()
+        steps = int(steps)
+        for i in range(steps):
+
+            # calculate pos for head
+            if direction == "R":
+                knots[0][0] += 1
+            elif direction == "L":
+                knots[0][0] -= 1
+            elif direction == "U":
+                knots[0][1] += 1
+            elif direction == "D":
+                knots[0][1] -= 1
+
+            # calculate pos for tail
+            for t in range(1, 10):
+                dx = knots[t-1][0] - knots[t][0]
+                dy = knots[t-1][1] - knots[t][1]
+
+                # same spot
+                if dx == dy == 0:
+                    pass
+
+                # direct neighbors
+                elif (
+                    (dx == 0 and abs(dy) == 1) or
+                    (abs(dx) == 1 and dy == 0) or
+                    (abs(dx) == abs(dy) == 1)
+                ):
+                    pass
+
+                # same col / 2 rows apart
+                elif dx == 0 and dy == 2:
+                    knots[t][1] += 1
+                elif dx == 0 and dy == -2:
+                    knots[t][1] -= 1
+
+                # same row / 2 cols apart
+                elif dx == 2 and dy == 0:
+                    knots[t][0] += 1
+                elif dx == -2 and dy == 0:
+                    knots[t][0] -= 1
+
+                # 1 col / 2 rows apart
+                elif abs(dx) == 1 and dy == 2:
+                    knots[t][0] = knots[t-1][0]
+                    knots[t][1] += 1
+                elif abs(dx) == 1 and dy == -2:
+                    knots[t][0] = knots[t-1][0]
+                    knots[t][1] -= 1
+
+                # 1 row / 2 cols apart
+                elif dx == 2 and abs(dy) == 1:
+                    knots[t][0] += 1
+                    knots[t][1] = knots[t-1][1]
+                elif dx == -2 and abs(dy) == 1:
+                    knots[t][0] -= 1
+                    knots[t][1] = knots[t-1][1]
+
+                # undefined
+                elif dx == 2 and dy == 2:
+                    knots[t][0] += 1
+                    knots[t][1] += 1
+                elif dx == 2 and dy == -2:
+                    knots[t][0] += 1
+                    knots[t][1] -= 1
+                elif dx == -2 and dy == 2:
+                    knots[t][0] -= 1
+                    knots[t][1] += 1
+                elif dx == -2 and dy == -2:
+                    knots[t][0] -= 1
+                    knots[t][1] -= 1
+
+            visited.add(tuple(knots[-1]))
+
+    return len(visited)
+
+
 assert task1(test_input) == 13
 print(task1(input_))
+
+assert task2(test_input) == 1
+assert task2(test_input2) == 36
+print(task2(input_))
