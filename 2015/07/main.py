bc7a94a25781a51e40b1dbcdb87804aa5a078916
Bastian Venthur <bastian.venthur@flixbus.com>
2022-12-12 15:28:59 +0100

    2015/7

--- 2015/07/main.py
+++ 2015/07/main.py
@@ -1,3 +1,5 @@
+import functools
+
 test_input = '''\
 123 -> x
 456 -> y
@@ -13,37 +15,78 @@ with open('input.txt') as fh:
     input_ = fh.read().splitlines()
 
 
-def task1(input_):
+def task1(input_, wire):
     wires = {}
 
     for line in input_:
-        left, out = line.split(' -> ')
-        if len(left.split()) == 1:
-            try:
-                value = int(left)
-                wires[out] = value
-            except ValueError:
-                wires[out] = wires.get(left, 0)
-        elif 'NOT' in left:
-            in_ = left.split()[-1]
-            wires[out] = ~wires.get(in_, 0)
-        else:
-            in1, op, in2 = left.split()
-            if op == 'AND':
-                wires[out] = wires.get(in1, 0) & wires.get(in2, 0)
-            elif op == 'OR':
-                wires[out] = wires.get(in1, 0) | wires.get(in2, 0)
-            elif op == 'LSHIFT':
-                wires[out] = wires.get(in1, 0) << int(in2)
-            elif op == 'RSHIFT':
-                wires[out] = wires.get(in1, 0) >> int(in2)
+        in_, out = line.split(' -> ')
+        wires[out] = in_
+
+    @functools.cache
+    def simulate(rule):
+        if len(rule.split()) == 1:
+            if rule.isdigit():
+                out = int(rule)
             else:
-                print(op)
-        wires[out] &= 0xffff
-    return wires
+                out = simulate(wires[rule])
+        elif 'NOT' in rule:
+            in_ = rule.split()[-1]
+            in_ = wires[in_]
+            out = ~simulate(in_)
+        else:
+            in1, op, in2 = rule.split()
+            if op == 'AND':
+                out = simulate(in1) & simulate(in2)
+            elif op == 'OR':
+                out = simulate(in1) | simulate(in2)
+            elif op == 'LSHIFT':
+                out = simulate(in1) << simulate(in2)
+            elif op == 'RSHIFT':
+                out = simulate(in1) >> simulate(in2)
+        return out & 0xffff
+
+    return simulate(wire)
 
 
-assert task1(test_input) == dict(
+def task2(input_):
+    wires = {}
+
+    for line in input_:
+        in_, out = line.split(' -> ')
+        wires[out] = in_
+
+    wires['b'] = '3176'
+
+    @functools.cache
+    def simulate(rule):
+        if len(rule.split()) == 1:
+            if rule.isdigit():
+                out = int(rule)
+            else:
+                out = simulate(wires[rule])
+        elif 'NOT' in rule:
+            in_ = rule.split()[-1]
+            in_ = wires[in_]
+            out = ~simulate(in_)
+        else:
+            in1, op, in2 = rule.split()
+            if op == 'AND':
+                out = simulate(in1) & simulate(in2)
+            elif op == 'OR':
+                out = simulate(in1) | simulate(in2)
+            elif op == 'LSHIFT':
+                out = simulate(in1) << simulate(in2)
+            elif op == 'RSHIFT':
+                out = simulate(in1) >> simulate(in2)
+        return out & 0xffff
+
+    return simulate('a')
+
+
+for k, v in dict(
     d=72, e=507, f=492, g=114, h=65412, i=65079, x=123, y=456
-)
-print(task1(input_)['a'])
+).items():
+    assert task1(test_input, k) == v
+print(task1(input_, 'a'))
+
+print(task2(input_))
