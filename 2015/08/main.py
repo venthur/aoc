0e88d8f7c8ad8dde1d49ebdd44b2f4e1156ca640
Bastian Venthur <venthur@debian.org>
2022-12-12 21:39:02 +0100

    2015/08 pt 2

--- 2015/08/main.py
+++ 2015/08/main.py
@@ -1,8 +1,3 @@
-import logging
-
-logging.basicConfig(level=logging.NOTSET)
-
-
 def task1(input_):
     with open(input_, 'rb') as fh:
         data = fh.read()
@@ -17,7 +12,6 @@ def task1(input_):
             break
         if c == '\n':
             continue
-        logging.debug(c)
         memory += 1
         if c == '\\':
             c2 = chr(next(data))
@@ -37,5 +31,33 @@ def task1(input_):
     return memory - string_literals
 
 
+def task2(input_):
+    with open(input_, 'rb') as fh:
+        data = fh.read()
+
+    data = iter(data)
+    string_literals = 0
+    memory = 0
+    while True:
+        try:
+            c = chr(next(data))
+        except StopIteration:
+            break
+        if c == '\n':
+            string_literals += 2
+            continue
+        memory += 1
+        print(c)
+        if c in ('\\', '"'):
+            string_literals += 2
+        else:
+            string_literals += 1
+    print(f'{string_literals=} {memory=}')
+    return string_literals - memory
+
+
 assert task1('test_input.txt') == 12
 print(task1('input.txt'))
+
+assert task2('test_input.txt') == 19
+print(task2('input.txt'))
