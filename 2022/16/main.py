1a7a4d9e67453d6a4dcef091434f9d6c7caab19c
Bastian Venthur <bastian.venthur@flixbus.com>
2022-12-16 17:24:12 +0100

    2022/16 pt 1

---
+++ 2022/16/main.py
@@ -0,0 +1,86 @@
+from itertools import permutations, pairwise
+import re
+
+
+def task1(fn):
+    with open(fn) as fh:
+        lines = fh.read().splitlines()
+
+    flow = dict()
+    conn = dict()
+    for line in lines:
+        print(line)
+        name, rate, connects = re.match(
+            r'^.*?([A-Z]{2,2}).*?(\d+).*?((?:[A-Z]{2,2}, )+[A-Z]{2,2}|[A-Z]{2,2})$',
+            line
+        ).groups()
+        rate = int(rate)
+        connects = [c.strip() for c in connects.split(',')]
+        flow[name] = rate
+        conn[name] = connects
+
+    pressurized = set()
+    # get the set of valves you'd want to open
+    for n, r in flow.items():
+        if r > 0:
+            pressurized.add(n)
+    print(pressurized)
+
+    # get the shortest distances between them
+    dist = dict()
+    for a in flow.keys():
+        stack = [(a, 0)]
+        visited = set()
+        while True:
+            try:
+                b, i = stack.pop(0)
+                visited.add(b)
+                i += 1
+            except IndexError:
+                break
+            for c in conn[b]:
+                if (a, c) in dist:
+                    d = dist[a, c]
+                    if i < d:
+                        dist[a, c] = i
+                        dist[c, a] = i
+                else:
+                    dist[a, c] = i
+                    dist[c, a] = i
+                if c not in visited:
+                    stack.append((c, i))
+    print(dist)
+
+    best_pressure = 0
+    # i suspect we don't need to go through all 15! permutations as we need
+    # some valves in between as well
+    for i, p in enumerate(permutations(pressurized, min(len(pressurized), 8))):
+        s_left = 30
+        pressure = 0
+
+        d = dist['AA', p[0]]
+        s_left -= d + 1
+        pressure += flow[p[0]] * s_left
+
+        jumps = 1
+        for a, b in pairwise(p):
+            jumps += 1
+            d = dist[a, b]
+            if d + 1 <= s_left:
+                s_left -= d + 1
+            else:
+                break
+            if s_left >= 1:
+                pressure += flow[b] * s_left
+            else:
+                break
+        print(f'Permutation: {i} {p} ({best_pressure=} {jumps=})')
+
+        if pressure > best_pressure:
+            best_pressure = pressure
+
+    return best_pressure
+
+
+assert task1('test_input.txt') == 1651
+print(task1('input.txt'))
