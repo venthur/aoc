8228a6d6e7dfe729b6bb560e4d3df3d5ef36c578
Bastian Venthur <bastian.venthur@flixbus.com>
2022-12-19 16:36:09 +0100

    2015/19 pt 1; pt2 WIP

---
+++ 2015/19/main.py
@@ -0,0 +1,71 @@
+from collections import deque
+from heapq import heappush, heappop
+
+def task1(fn):
+
+    with open(fn) as fh:
+        data = fh.read().splitlines()
+    rules_raw = data[:-2]
+    molecule = data[-1]
+
+    rules = []
+    for rule in rules_raw:
+        rules.append(rule.split(' => '))
+
+    distinct = set()
+    for in_, out in rules:
+        i = 0
+        while True:
+            i = molecule.find(in_, i)
+            if i == -1:
+                break
+            new = molecule[:i] + out + molecule[i+len(in_):]
+            distinct.add(new)
+            i += 1
+
+    return len(distinct)
+
+
+def task2(fn):
+
+    with open(fn) as fh:
+        data = fh.read().splitlines()
+    rules_raw = data[:-2]
+    molecule = data[-1]
+
+    rules = []
+    for rule in rules_raw:
+        rules.append(rule.split(' => '))
+
+    print(rules)
+    rules.sort(key=lambda x: len(x[1]), reverse=True)
+    print(rules)
+
+    h = []
+    heappush(h, (len(molecule), 0, molecule))
+    seen = set()
+    while True:
+        _, steps, current = heappop(h)
+        print(f'{len(current)=} {len(h)=}, {len(seen)=}')
+        if current == 'e':
+            return steps
+        for in_, out in rules:
+            i = 0
+            while True:
+                i = current.find(out, i)
+                if i == -1:
+                    break
+                new = current[:i] + in_ + current[i+len(out):]
+                if new not in seen:
+                    heappush(h, (len(new), steps+1, new))
+                    seen.add(new)
+                i += 1
+
+
+#assert task1('test_input.txt') == 4
+#assert task1('test_input2.txt') == 7
+#print(task1('input.txt'))
+
+assert task2('test_input3.txt') == 3
+assert task2('test_input4.txt') == 6
+print(task2('input.txt'))
