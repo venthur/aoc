708be7088a57a6a8f9ba2b51844637c347856872
Bastian Venthur <venthur@debian.org>
2022-12-24 13:14:00 +0100

    pt 2

--- 2016/10/main.py
+++ 2016/10/main.py
@@ -1,6 +1,7 @@
 import re
 from collections import defaultdict
 
+
 def task1(fn, values):
     with open(fn) as fh:
         lines = fh.read().splitlines()
@@ -36,5 +37,43 @@ def task1(fn, values):
                 agents[high].append(hv)
 
 
+def task2(fn):
+    with open(fn) as fh:
+        lines = fh.read().splitlines()
+
+    rules = dict()
+    agents = defaultdict(list)
+
+    init = re.compile(r'^value (\d+) goes to (.*?)$')
+    process = re.compile(r'^(.*?) gives low to (.*?) and high to (.*?)$')
+    for line in lines:
+        if m := init.match(line):
+            v, agent = m.groups()
+            agents[agent].append(int(v))
+        elif m := process.match(line):
+            agent, low, high = m.groups()
+            rules[agent] = (low, high)
+        else:
+            raise ValueError()
+
+    print(agents.keys())
+    while True:
+        for agent, (low, high) in rules.items():
+            if not agent.startswith('bot'):
+                continue
+            for i in range(3):
+                if agents[f'output {i}'] == []:
+                    break
+            else:
+                return agents['output 0'][0] * agents['output 1'][0] * agents['output 2'][0]
+            if len(agents[agent]) >= 2:
+                lv, hv = sorted(agents[agent][:2])
+                agents[agent] = agents[agent][2:]
+                agents[low].append(lv)
+                agents[high].append(hv)
+
+
 assert task1('test_input.txt', (5, 2)) == 2
 print(task1('input.txt', (17, 61)))
+
+print(task2('input.txt'))
