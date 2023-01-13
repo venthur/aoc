3d631bbe2e4f457ba93790354feb767bb7e407dc
Bastian Venthur <bastian.venthur@flixbus.com>
2023-01-13 17:03:12 +0100

    2017/16 pt 1, pt 2 wip

---
+++ 2017/16/main.py
@@ -0,0 +1,64 @@
+def task1(fn):
+    with open(fn) as fh:
+        data = fh.read().strip()
+
+    programs = list('abcdefghijklmnop')
+    data = data.split(',')
+    for move in data:
+        c, p = move[0], move[1:]
+        if c == 's':
+            p = int(p)
+            programs = programs[-p:] + programs[:-p]
+        elif c == 'x':
+            a, b = [int(i) for i in p.split('/')]
+            c = programs[a]
+            programs[a] = programs[b]
+            programs[b] = c
+        elif c == 'p':
+            a, b = [i for i in p.split('/')]
+            ai = programs.index(a)
+            bi = programs.index(b)
+            programs[ai] = b
+            programs[bi] = a
+    return ''.join(programs)
+
+
+def task2(fn):
+    with open(fn) as fh:
+        data = fh.read().strip()
+
+    programs = list('abcdefghijklmnop')
+    data = data.split(',')
+    data2 = []
+    for move in data:
+        c, p = move[0], move[1:]
+        if c == 's':
+            data2.append((c, int(p)))
+        elif c == 'x':
+            data2.append((c, [int(i) for i in p.split('/')]))
+        elif c == 'p':
+            data2.append((c, [i for i in p.split('/')]))
+    data = data2[:]
+
+    #for i in range(1):
+    for i in range(1000000000):
+        if i % 1000 == 0:
+            print(f'{100*i/1000000000}')
+        for cmd, params in data:
+            if cmd == 's':
+                programs = programs[-params:] + programs[:-params]
+            elif cmd == 'x':
+                c = programs[params[0]]
+                programs[params[0]] = programs[params[1]]
+                programs[params[1]] = c
+            elif cmd == 'p':
+                ai = programs.index(params[0])
+                bi = programs.index(params[1])
+                programs[ai] = params[1]
+                programs[bi] = params[0]
+
+    return ''.join(programs)
+
+
+print(task1('input.txt'))
+print(task2('input.txt'))
