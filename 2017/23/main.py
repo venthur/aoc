1e38a8c916b724422bf24d4b98deb1846fcc045e
Bastian Venthur <bastian.venthur@flixbus.com>
2023-01-18 08:24:15 +0100

    2017/23

--- 2017/23/main.py
+++ 2017/23/main.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 def task1(fn):
     with open(fn) as fh:
         lines = fh.read().splitlines()
@@ -37,48 +40,26 @@ def task1(fn):
     return multiplications
 
 
-def task2(fn):
-    with open(fn) as fh:
-        lines = fh.read().splitlines()
+def is_prime(n):
+    if n == 1:
+        return False
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-    def convert_int(s):
-        if s.startswith('-') or s.isnumeric():
-            return int(s)
-        return s
 
-    lines = [list(map(convert_int, line.split())) for line in lines]
+def task2():
+    b = 109300
+    c = 126300
+    h = 0
+    for b in range(b, c+1, 17):
+        if not is_prime(b):
+            h += 1
 
-    ptr = 0
-    i = 0
-    register = dict(a=1, b=0, c=0, d=0, e=0, f=0, g=0, h=0)
-    while 0 <= ptr < len(lines):
-        if i % 10000000 == 0:
-            print(f'{register}')
-            i = 0
-        i += 1
-        match lines[ptr]:
-            case 'set', reg, val:
-                if isinstance(val, str):
-                    val = register[val]
-                register[reg] = val
-            case 'sub', reg, val:
-                if isinstance(val, str):
-                    val = register[val]
-                register[reg] -= val
-            case 'mul', reg, val:
-                if isinstance(val, str):
-                    val = register[val]
-                register[reg] *= val
-            case 'jnz', val, offset:
-                if isinstance(val, str):
-                    val = register[val]
-                if val != 0:
-                    ptr += offset - 1
-        ptr += 1
-
-    return register['h']
+    return h
 
 
 print(task1('input.txt'))
 
-print(task2('input.txt'))
+print(task2())
