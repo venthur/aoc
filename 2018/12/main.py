83c3bf7a14f3a8d0154054f8b07545fefe4afbaf
Bastian Venthur <venthur@debian.org>
2023-01-23 21:49:21 +0100

    2018/12 pt 1

---
+++ 2018/12/main.py
@@ -0,0 +1,42 @@
+def task1(fn, generations):
+    with open(fn) as fh:
+        lines = fh.read().splitlines()
+
+    state = {i: c for i, c in enumerate(lines[0].split()[-1])}
+
+    rules = []
+    lines = lines[2:]
+    for line in lines:
+        pattern, result = line.split(' => ')
+        rules.append((pattern, result))
+
+    for i in range(generations):
+
+        if i % 1000 == 0:
+            print(i)
+
+        while ''.join([v for _, v in sorted(state.items())][:2]) != '..':
+            state[min(state.keys())-1] = '.'
+
+        while ''.join([v for _, v in sorted(state.items())][-2:]) != '..':
+            state[max(state.keys())+1] = '.'
+
+        state_new = {i: '.' for i in state.keys()}
+
+        for j in list(state.keys()):
+            p = state.get(j-2, '.') + state.get(j-1, '.') + state[j] + state.get(j+1, '.') + state.get(j+2, '.')
+            for pattern, result in rules:
+                if p == pattern:
+                    state_new[j] = result
+                    break
+        state = state_new.copy()
+
+    s = sum(map(lambda x: x[0], filter(lambda x: x[1] == '#', state.items())))
+
+    return s
+
+
+assert task1('test_input.txt', 20) == 325
+print(task1('input.txt', 20))
+
+print(task1('input.txt', 50000000000))
