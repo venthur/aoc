ffbd8f769e08b286f6b79083d65b539954d3efbf
Bastian Venthur <venthur@debian.org>
2023-01-26 19:21:44 +0100

    pt2

--- 2018/16/main.py
+++ 2018/16/main.py
@@ -120,10 +120,61 @@ def task1(fn):
         ):
             if op(before, *cmd[1:]) == after:
                 possible += 1
-                print(f'{op} {before} {cmd[1:]} -> {after}')
         possible_opcodes.append(possible)
 
     return len(list(filter(lambda x: x >= 3, possible_opcodes)))
 
 
+def task2(fn):
+    with open(fn) as fh:
+        data = fh.read()
+
+    lines1, lines2 = data.split('\n\n\n\n')
+
+    samples = []
+    for line in lines1.splitlines():
+        if line.startswith('Before'):
+            before = eval(line.split(': ')[-1])
+        elif line.startswith('After'):
+            after = eval(line.split(': ')[-1])
+            samples.append((before, after, cmd))
+        else:
+            cmd = [int(i) for i in line.split()]
+
+    commands = []
+    for line in lines2.splitlines():
+        commands.append([int(i) for i in line.split()])
+
+    opcode = {
+        i: {
+            addr, addi, mulr, muli,
+            banr, bani, borr, bori,
+            setr, seti,
+            getir, getri, getrr,
+            equir, equri, equrr
+        } for i in range(16)
+    }
+
+    for before, after, cmd in samples:
+        discard = set()
+        for op in opcode[cmd[0]]:
+            if op(before, *cmd[1:]) != after:
+                discard.add(op)
+        for n in discard:
+            opcode[cmd[0]].discard(n)
+        if len(opcode[cmd[0]]) == 1:
+            for op, fs in opcode.items():
+                if op != cmd[0]:
+                    fs.difference_update(opcode[cmd[0]])
+
+    opcode = {k: v.pop() for k, v in opcode.items()}
+
+    registers = [0 for i in range(4)]
+    for op, *params in commands:
+        registers = opcode[op](registers, *params)
+
+    return registers[0]
+
+
 print(task1('input.txt'))
+print(task2('input.txt'))
