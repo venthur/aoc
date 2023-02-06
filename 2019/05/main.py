295817fd4aa25c6a0d7e2de20c6b881c67b567ad
Bastian Venthur <bastian.venthur@flixbus.com>
2023-02-06 10:29:49 +0100

    2019/05

--- 2019/05/main.py
+++ 2019/05/main.py
@@ -1,43 +1,61 @@
 def task1(fn, in_):
     with open(fn) as fh:
-        code = fh.read().strip().split(',')
+        code = [int(n) for n in fh.read().strip().split(',')]
 
-    print(code)
-
-    io = [str(in_), None]
+    io = [in_, None]
     ptr = 0
     while True:
-        # print(code, ptr)
-        cmd = code[ptr]
-        cmd = '0' * (5 - len(cmd)) + cmd
-        match int(code[ptr][-2:]):
+        instr = code[ptr]
+        op = instr % 100
+        if (len(code) - ptr) > 3:
+            c = code[ptr+3]
+            ci = instr // 10000 % 10
+        if (len(code) - ptr) > 2:
+            b = code[ptr+2]
+            bi = instr // 1000 % 10
+        if (len(code) - ptr) > 1:
+            a = code[ptr+1]
+            ai = instr // 100 % 10
+
+        # print(f'{ptr=} {code[ptr]=}, {a, b, c=} {ai, bi, ci=} {io=}')
+        match op:
             case 1:
-                a, b, c = [int(n) for n in code[ptr+1:ptr+1+3]]
-                if cmd[0] == '0':
-                    a = int(code[a])
-                if cmd[1] == '0':
-                    b = int(code[b])
-                code[c] = str(a + b)
+                a = code[a] if not ai else a
+                b = code[b] if not bi else b
+                code[c] = a + b
                 ptr += 4
             case 2:
-                a, b, c = [int(n) for n in code[ptr+1:ptr+1+3]]
-                if cmd[0] == '0':
-                    a = int(code[a])
-                if cmd[1] == '0':
-                    b = int(code[b])
-                code[c] = str(a * b)
+                a = code[a] if not ai else a
+                b = code[b] if not bi else b
+                code[c] = a * b
                 ptr += 4
             case 3:
-                c = code[ptr+1]
-                code[int(c)] = str(io[0])
+                code[a] = io[0]
                 ptr += 2
             case 4:
-                c = int(code[ptr+1])
-                if cmd[2] == '0':
-                    c = int(code[c])
-                io[1] = str(c)
-                print(io)
+                a = code[a] if not ai else a
+                io[1] = a
                 ptr += 2
+            case 5:
+                a = code[a] if not ai else a
+                b = code[b] if not bi else b
+                ptr = b-3 if a else ptr
+                ptr += 3
+            case 6:
+                a = code[a] if not ai else a
+                b = code[b] if not bi else b
+                ptr = b-3 if not a else ptr
+                ptr += 3
+            case 7:
+                a = code[a] if not ai else a
+                b = code[b] if not bi else b
+                code[c] = 1 if a < b else 0
+                ptr += 4
+            case 8:
+                a = code[a] if not ai else a
+                b = code[b] if not bi else b
+                code[c] = 1 if a == b else 0
+                ptr += 4
             case 99:
                 break
 
@@ -45,3 +63,4 @@ def task1(fn, in_):
 
 
 print(task1('input.txt', 1))
+print(task1('input.txt', 5))
