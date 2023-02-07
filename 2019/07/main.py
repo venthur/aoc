c8662fece4bdc5d83ce3ba08b9b8b82843446bfe
Bastian Venthur <bastian.venthur@flixbus.com>
2023-02-07 15:38:42 +0100

    refactoring

--- 2019/07/main.py
+++ 2019/07/main.py
@@ -8,62 +8,60 @@ def compute(index, phase, code, io):
     while True:
         instr = code[ptr]
         op = instr % 100
-        if (len(code) - ptr) > 3:
-            c = code[ptr+3]
-            ci = instr // 10000 % 10
-        if (len(code) - ptr) > 2:
-            b = code[ptr+2]
-            bi = instr // 1000 % 10
-        if (len(code) - ptr) > 1:
-            a = code[ptr+1]
+        if op in (3, 4):
+            ap = code[ptr+1]
             ai = instr // 100 % 10
+            a = code[ap] if not ai else ap
+        if op in (5, 6):
+            ap = code[ptr+1]
+            ai = instr // 100 % 10
+            a = code[ap] if not ai else ap
+            bp = code[ptr+2]
+            bi = instr // 1000 % 10
+            b = code[bp] if not bi else bp
+        if op in (1, 2, 7, 8):
+            ap = code[ptr+1]
+            ai = instr // 100 % 10
+            a = code[ap] if not ai else ap
+            bp = code[ptr+2]
+            bi = instr // 1000 % 10
+            b = code[bp] if not bi else bp
+            cp = code[ptr+3]
+            ci = instr // 10000 % 10
+            c = code[cp] if not ci else cp
 
         # print(f'{ptr=} {code[ptr]=}, {a, b, c=} {ai, bi, ci=} {io=}')
         match op:
             case 1:
-                a = code[a] if not ai else a
-                b = code[b] if not bi else b
-                code[c] = a + b
+                code[cp] = a + b
                 ptr += 4
             case 2:
-                a = code[a] if not ai else a
-                b = code[b] if not bi else b
-                code[c] = a * b
+                code[cp] = a * b
                 ptr += 4
             case 3:
                 if first:
-                    code[a] = phase
+                    code[ap] = phase
                     first = False
                 else:
-                    code[a] = io[index]
+                    code[ap] = io[index]
                 ptr += 2
             case 4:
-                a = code[a] if not ai else a
                 io[index+1] = a
                 ptr += 2
             case 5:
-                a = code[a] if not ai else a
-                b = code[b] if not bi else b
                 ptr = b-3 if a else ptr
                 ptr += 3
             case 6:
-                a = code[a] if not ai else a
-                b = code[b] if not bi else b
                 ptr = b-3 if not a else ptr
                 ptr += 3
             case 7:
-                a = code[a] if not ai else a
-                b = code[b] if not bi else b
-                code[c] = 1 if a < b else 0
+                code[cp] = 1 if a < b else 0
                 ptr += 4
             case 8:
-                a = code[a] if not ai else a
-                b = code[b] if not bi else b
-                code[c] = 1 if a == b else 0
+                code[cp] = 1 if a == b else 0
                 ptr += 4
             case 99:
                 break
-
 
 
 def task1(fn):
@@ -76,7 +74,6 @@ def task1(fn):
 
         for i, phase in enumerate(p):
             compute(i, phase, code, io)
-            print(io)
 
         if maxv is None or io[-1] > maxv:
             maxv = io[-1]
