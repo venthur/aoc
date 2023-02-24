368d593bb0194175b7baba0bda050e143ffc1745
Bastian Venthur <bastian.venthur@flixbus.com>
2023-02-24 08:46:52 +0100

    2020/13

--- 2020/13/main.py
+++ 2020/13/main.py
@@ -14,5 +14,26 @@ def task1(fn):
     return (earliest - ts) * id_
 
 
+def task2(fn):
+    with open(fn) as fh:
+        ts, ids = fh.read().splitlines()
+
+    busses = [(i, int(n)) for i, n in enumerate(ids.split(',')) if n.isnumeric()]
+
+    d = 1
+    i = 0
+    for offset, bus in busses:
+        while True:
+            i += d
+            if (i + offset) % bus == 0:
+                d *= bus
+                break
+
+    return i
+
+
 assert task1('test_input0.txt') == 295
 print(task1('input.txt'))
+
+assert task2('test_input0.txt') == 1068781
+print(task2('input.txt'))
