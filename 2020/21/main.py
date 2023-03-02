02d282bf2601cbb5c2fda0ee4f98fdc672a3d5cb
Bastian Venthur <bastian.venthur@flixbus.com>
2023-03-02 10:34:25 +0100

    2020/21

--- 2020/21/main.py
+++ 2020/21/main.py
@@ -2,7 +2,6 @@ def task1(fn):
     with open(fn) as fh:
         lines = fh.read().splitlines()
 
-    tmp = dict()
     foods = []
     for line in lines:
         line = line.replace('(', '')
@@ -11,7 +10,6 @@ def task1(fn):
         ingrediens, allergens = line.split('contains')
         foods.append([set(ingrediens.split()), set(allergens.split())])
 
-    print(foods)
     allergens = dict()
     for ing, algs in foods:
         for a in algs:
@@ -19,8 +17,32 @@ def task1(fn):
                 allergens[a] = ing.copy()
             else:
                 allergens[a].intersection_update(ing)
-    print(allergens)
+
+    while not all(len(f) == 1 for f in allergens.values()):
+        for k, v in allergens.items():
+            if len(v) == 1:
+                for ki, vi in allergens.items():
+                    if ki == k:
+                        continue
+                    vi.difference_update(v)
+
+    count = 0
+    afoods = set()
+    for k in allergens.values():
+        afoods.update(k)
+    for ing, _ in foods:
+        for i in ing:
+            if i not in afoods:
+                count += 1
+
+    pt1 = count
+
+    l = [(k, v.pop())for k, v in allergens.items()]
+    l.sort()
+    pt2 = ','.join([l[1] for l in l])
+
+    return pt1, pt2
 
 
-assert task1('test_input0.txt') == 5
+assert task1('test_input0.txt') == (5, 'mxmxvkd,sqjhc,fvjkl')
 print(task1('input.txt'))
