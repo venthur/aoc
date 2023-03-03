d2bde648ce143468be040c9571e955a389ca361e
Bastian Venthur <bastian.venthur@flixbus.com>
2023-03-03 17:17:44 +0100

    wip

--- 2021/03/main.py
+++ 2021/03/main.py
@@ -18,5 +18,22 @@ def task1(fn):
     return v * v2
 
 
+def task2(fn):
+    with open(fn) as fh:
+        lines = fh.read().splitlines()
+
+    l = len(lines[0])
+    n = ''.join(lines)
+    print(n)
+
+    for i in range(l):
+        n[i::l]
+
+    return ox * co
+
+
 assert task1('test_input.txt') == 198
 print(task1('input.txt'))
+
+assert task2('test_input.txt') == 230
+print(task2('input.txt'))
