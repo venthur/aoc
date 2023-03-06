58735054df116b2b181c77af760828b75431b8b4
Bastian Venthur <venthur@debian.org>
2023-03-06 22:21:04 +0100

    wip

---
+++ 2021/15/main.py
@@ -0,0 +1,31 @@
+from math import dist
+from heapq import heappush, heappop
+
+
+def task1(fn):
+    with open(fn) as fh:
+        lines = fh.read().splitlines()
+
+    risklevel = dict()
+    for y, line in enumerate(lines):
+        for x, char in enumerate(line):
+            risklevel[x, y] = int(char)
+    goal = (x, y)
+
+    todo = []
+    heappush(todo, (0, 0, 0, 0))
+    visited = {(0, 0)}
+    while todo:
+        _, x, y, cost = heappop(todo)
+        print(x, y, cost)
+        if (x, y) == goal:
+            return cost
+        for xi, yi in (x+1, y), (x-1, y), (x, y+1), (x, y-1):
+            if (xi, yi) in risklevel and (xi, yi) not in visited:
+                visited.add((xi, yi))
+                r = risklevel[xi, yi]
+                heappush(todo, (cost+r+dist((xi, yi), goal), xi, yi, cost+r))
+
+
+assert task1('test_input0.txt') == 40
+print(task1('input.txt'))
