a7eecb12076275ce47e698cdcbab88e908281a7b
Bastian Venthur <bastian.venthur@flixbus.com>
2023-12-04 12:47:07 +0100

    2023/03

--- 2023/03/main.py
+++ 2023/03/main.py
@@ -6,38 +6,74 @@ def task1(fn):
     data = defaultdict(str)
     with open(fn) as fh:
         for row, line in enumerate(fh.read().splitlines()):
-            n = None
             for col, char in enumerate(line):
-                if char.isnumeric():
-                    if n is None:
-                        n = ''
-                    n = n + char
-                    continue
-                else:
-                    if n is None:
-                        pass
-                    else:
-                        ndigits = len(str(n))
-                        data[(row, col-ndigits)] = n
-                        n = None
-                if char != '.':
+                if char.isdecimal():
+                    if line[col-1].isdecimal():
+                        continue
+                    # read characters until we have a number
+                    value = char
+                    i = 1
+                    while col+i <= len(line)-1 and line[col+i].isdecimal():
+                        value = value + line[col+i]
+                        i += 1
+                    data[(row, col)] = value
+                elif char != '.':
                     data[(row, col)] = char
-
 
     # check all numbers if they have non numeric neighbors
     s = 0
-    for (row, col), val in filter(lambda x: x[-1].isnumeric(), list(data.items())):
+    for (row, col), val in filter(
+        lambda x: x[-1].isdecimal(), list(data.items())
+    ):
         digits = len(val)
-        for x, y in product(range(digits+2), (-1, 0, 1)):
-            c = data[(row+y, col-1+x)]
-            if c and not c.isnumeric():
+        for x, y in product(range(col-1, col+1+digits), range(row-1, row+2)):
+            c = data[(y, x)]
+            if c and not c.isdecimal():
                 s += int(val)
-                print(val)
                 break
+        else:
+            pass
 
-    print(s)
+    return s
+
+
+def task2(fn):
+    data = defaultdict(str)
+    with open(fn) as fh:
+        for row, line in enumerate(fh.read().splitlines()):
+            for col, char in enumerate(line):
+                if char.isdecimal():
+                    if line[col-1].isdecimal():
+                        continue
+                    # read characters until we have a number
+                    value = char
+                    i = 1
+                    while col+i <= len(line)-1 and line[col+i].isdecimal():
+                        value = value + line[col+i]
+                        i += 1
+                    data[(row, col)] = value
+                elif char == '*':
+                    data[(row, col)] = char
+
+    gears = {}
+    for (row, col), val in filter(
+        lambda x: x[-1].isdecimal(), list(data.items())
+    ):
+        digits = len(val)
+        for x, y in product(range(col-1, col+1+digits), range(row-1, row+2)):
+            c = data[(y, x)]
+            if c and not c.isdecimal():
+                gears[(y, x)] = gears.get((y, x), []) + [val]
+
+    s = 0
+    for (row, col), vals in gears.items():
+        if len(vals) == 2:
+            s += int(vals[0]) * int(vals[1])
     return s
 
 
 assert task1('test_input.txt') == 4361
 print(task1('input.txt'))
+
+assert task2('test_input.txt') == 467835
+print(task2('input.txt'))
