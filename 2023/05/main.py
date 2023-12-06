6adac367312a5da870d4b3eaa56978337a320850
Bastian Venthur <bastian.venthur@flixbus.com>
2023-12-06 12:58:09 +0100

    2023/05 pt 2

--- 2023/05/main.py
+++ 2023/05/main.py
@@ -1,4 +1,4 @@
-from itertools import combinations
+from sys import maxsize
 
 
 def task1(fn):
@@ -37,33 +37,7 @@ def task2(fn):
         blocks = fh.read().split('\n\n')
 
     seed_line = [int(i) for i in blocks[0].split(':')[-1].strip().split()]
-
-    print(f'naive: {sum(seed_line[1::2])}')
-
-    # merge ranges overlapping ranges until no more overlaps
     ranges = list(zip(seed_line[::2], seed_line[1::2]))
-    while True:
-        for a, b in combinations(ranges, 2):
-            print(f'trying {a, b}')
-            if b[0] <= a[0] < b[0] + b[1]:
-                c = [b[0], max(b[1], a[0] + a[1] - b[0])]
-                ranges.remove(a)
-                ranges.remove(b)
-                ranges.append(c)
-                print(c)
-                break
-            if a[0] <= b[0] < a[0] + a[1]:
-                c = [a[0], max(a[1], b[0] + b[1] - a[0])]
-                ranges.remove(a)
-                ranges.remove(b)
-                ranges.append(c)
-                print(c)
-                break
-        else:
-            break
-    print(f'final ranges: {ranges}')
-    print(f'merged: {sum(r[1] for r in ranges)}')
-    return
 
     maps = []
     for block in blocks[1:]:
@@ -74,20 +48,48 @@ def task2(fn):
             map_.append(numbers)
         maps.append(map_)
 
+    # find endpoints
+    endpoints = {0, maxsize}
+    for map_ in reversed(maps):
+
+        new_endpoints = set()
+        for dst_start, src_start, range_len in map_:
+            new_endpoints.add(src_start)
+            new_endpoints.add(src_start + range_len)
+            new_endpoints.add(src_start-1)
+            new_endpoints.add(src_start + range_len-1)
+
+        # remap endpoints
+        for p in endpoints:
+            for dst_start, src_start, range_len in map_:
+                if dst_start <= p < dst_start + range_len:
+                    p = src_start + (p - dst_start)
+                    break
+            new_endpoints.add(p)
+        endpoints = new_endpoints
+
+    endpoints = sorted(endpoints)
+
+    # remap seeds
+    candidate_endpoints = set()
+    for start, r in ranges:
+        for p in (start, start+r-1):
+            points = set(filter(lambda x: start <= x <= start+r-1, endpoints))
+            points.add(start)
+            points.add(start+r-1)
+            candidate_endpoints = candidate_endpoints.union(points)
+
     min_loc = None
-    for a, b in zip(seed_line[::2], seed_line[1::2]):
-        for i in range(b):
-            seed = a + i
-            for map_ in maps:
-                for dst_start, src_start, range_len in map_:
-                    if src_start <= seed < src_start + range_len:
-                        gap = seed - src_start
-                        seed = dst_start + gap
-                        break
-            if min_loc is None:
-                min_loc = seed
-            else:
-                min_loc = min(min_loc, seed)
+    for seed in candidate_endpoints:
+        for map_ in maps:
+            for dst_start, src_start, range_len in map_:
+                if src_start <= seed < src_start + range_len:
+                    seed = dst_start + (seed - src_start)
+                    break
+        if min_loc is None:
+            min_loc = seed
+        else:
+            min_loc = min(min_loc, seed)
 
     return min_loc
 
