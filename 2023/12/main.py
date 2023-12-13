9951c6cd73e16fe9da480eca041eb44a173d134e
Bastian Venthur <venthur@debian.org>
2023-12-13 20:15:08 +0100

    pt2

--- 2023/12/main.py
+++ 2023/12/main.py
@@ -1,39 +1,43 @@
-def is_valid(conditions, groups):
-    conditions = [c for c in conditions.split('.') if c]
-    if len(conditions) != len(groups):
-        return False
-    for c, g in zip(conditions, groups):
-        if len(c) != g:
-            return False
-    return True
+from functools import cache
 
 
+@cache
 def get_arrangements(conditions, groups):
 
-    if '?' not in conditions:
-        if is_valid(conditions, groups):
+    if not groups:
+        if '#' not in conditions:
             return 1
         else:
             return 0
 
-    # check if conditions and groups are possible
-    conditions2 = [c for c in conditions.split('.') if c]
-    for c, g in zip(conditions2, groups):
-        if '?' in c:
-            break
-        if len(c) != g:
+    if not conditions:
+        return 0
+
+    next_group = groups[0]
+
+    if conditions[0] == '.':
+        return get_arrangements(conditions[1:], groups)
+    elif conditions[0] == '#':
+        this_group = conditions[:next_group]
+        this_group = this_group.replace('?', '#')
+        if this_group != next_group * '#':
             return 0
 
-    for c, g in zip(reversed(conditions2), reversed(groups)):
-        if '?' in c:
-            break
-        if len(c) != g:
-            return 0
+        if len(conditions) == next_group:
+            if len(groups) == 1:
+                return 1
+            else:
+                return 0
 
-    return (
-        get_arrangements(conditions.replace('?', '.', 1), groups) +
-        get_arrangements(conditions.replace('?', '#', 1), groups)
-    )
+        if conditions[next_group] in '?.':
+            return get_arrangements(conditions[next_group+1:], groups[1:])
+
+        return 0
+    elif conditions[0] == '?':
+        return (
+            get_arrangements(conditions.replace('?', '.', 1), groups) +
+            get_arrangements(conditions.replace('?', '#', 1), groups)
+        )
 
 
 def task1(fn):
@@ -41,7 +45,7 @@ def task1(fn):
     with open(fn) as fh:
         for line in fh.read().splitlines():
             conditions, groups = line.split()
-            groups = [int(x) for x in groups.split(',')]
+            groups = tuple([int(x) for x in groups.split(',')])
             arrangements += get_arrangements(conditions, groups)
 
     return arrangements
@@ -51,11 +55,10 @@ def task2(fn):
     arrangements = 0
     with open(fn) as fh:
         for i, line in enumerate(fh.read().splitlines()):
-            print(i)
             conditions, groups = line.split()
             conditions = '?'.join([conditions] * 5)
             groups = ','.join([groups] * 5)
-            groups = [int(x) for x in groups.split(',')]
+            groups = tuple([int(x) for x in groups.split(',')])
             arrangements += get_arrangements(conditions, groups)
 
     return arrangements
@@ -64,5 +67,5 @@ def task2(fn):
 assert task1('test_input.txt') == 21
 print(task1('input.txt'))
 
-#assert task2('test_input.txt') == 525152
-#print(task2('input.txt'))
+assert task2('test_input.txt') == 525152
+print(task2('input.txt'))
