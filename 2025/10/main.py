5f1e2570ec295e8cbf97ca4e712f2736e4f38326
Bastian Venthur <bastian.venthur@flix.com>
2025-12-10 16:14:53 +0100

    2025/10 pt1

---
+++ 2025/10/main.py
@@ -0,0 +1,112 @@
+from collections import deque
+
+
+def task1(fn):
+    schematics = []
+    with open(fn) as fh:
+        for line in fh:
+            line = line.strip()
+
+            data, rest = line.split(']')
+            data = data[1:]
+            lights = [True if i == '#' else False for i in data]
+
+            data1, data2 = rest.split('{')
+            data1 = data1.strip()
+            buttons = []
+            for d in data1.split():
+                d = d[1:-1]
+                button = [int(i) for i in d.split(',')]
+                buttons.append(button)
+
+            data = data2[:-1]
+            joltages = [int(i) for i in data.split(',')]
+
+            schematics.append([lights, buttons, joltages])
+
+    result = 0
+    for lights, buttons, joltages in schematics:
+        seen = set()
+        queue = []
+        l = [False for i in range(len(lights))]
+        queue.append([0, l])
+        seen.add(tuple(l))
+
+        while queue:
+            presses, l = queue.pop(0)
+            if l == lights:
+                result += presses
+                break
+            for button in buttons:
+                lnew = l[:]
+                for bi in button:
+                    lnew[bi] = not lnew[bi]
+                if tuple(lnew) not in seen:
+                    queue.append([presses+1, lnew])
+                    seen.add(tuple(lnew))
+        else:
+            raise ValueError('Empty Queue')
+
+    return result
+
+
+def task2(fn):
+    schematics = []
+    with open(fn) as fh:
+        for line in fh:
+            line = line.strip()
+
+            data, rest = line.split(']')
+            data = data[1:]
+            lights = [True if i == '#' else False for i in data]
+
+            data1, data2 = rest.split('{')
+            data1 = data1.strip()
+            buttons = []
+            for d in data1.split():
+                d = d[1:-1]
+                button = [int(i) for i in d.split(',')]
+                buttons.append(button)
+
+            data = data2[:-1]
+            joltages = [int(i) for i in data.split(',')]
+
+            schematics.append([lights, buttons, joltages])
+
+
+    INF = 2**32
+    from functools import cache
+
+    @cache
+    def calculate_button_presses(buttonpresses, joltages):
+
+        if all([i == 0 for i in joltages]):
+            return buttonpresses
+
+        if any([i < 0 for i in joltages]):
+            return INF
+
+        min_presses = INF
+        for button in buttons:
+            joltages2 = list(joltages[:])
+            for bi in button:
+                joltages2[bi] -= 1
+            p = calculate_button_presses(buttonpresses+1, tuple(joltages2))
+            if p < min_presses:
+                min_presses = p
+
+        return min_presses
+
+    result = 0
+    for lights, buttons, joltages in schematics:
+        calculate_button_presses.cache_clear()
+        result += calculate_button_presses(0, tuple(joltages))
+        print('found')
+
+    return result
+
+assert task1('test_input.txt') == 7
+print(task1('input.txt'))
+
+assert task2('test_input.txt') == 33
+print(task2('input.txt'))
